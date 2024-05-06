16ac17d6fad577938e22736cc8bf3c99ecf2dc81
petroniodias <63084961+petroniodias@users.noreply.github.com>
2024-05-06 19:45:12 -0300

    Update RevisaoArray.java

--- Java/RevisaoArray.java
+++ Java/RevisaoArray.java
@@ -4,7 +4,7 @@ public class RevisaoArray {
     public static void main (String[] args){
         Scanner teclado = new Scanner(System.in);
 
-        int quantidade = 4; // Quantidade de alunos no vetor
+        int quantidade = 15; // Quantidade de alunos no vetor
         String[] nome = new String[quantidade];
         double[] nota1 = new double[quantidade];
         double[] nota2 = new double[quantidade];
@@ -14,6 +14,10 @@ public class RevisaoArray {
         while(i<quantidade){
             System.out.print("Digitite o nome: ");
             nome[i] = teclado.nextLine();
+            if (nome[i].equalsIgnoreCase("fim")){
+                break;
+            }
+
             System.out.print("Digitite o nota 1: ");
             nota1[i] = teclado.nextDouble();
             System.out.print("Digitite o nota 2: ");
@@ -24,12 +28,12 @@ public class RevisaoArray {
         teclado.close();
 
         somaMedia = 0;
-        for(i=0;i<quantidade;i++){
-            media = (nota1[i]+nota2[i])/2;
+        for(int j=0;j<i;j++){
+            media = (nota1[j]+nota2[j])/2;
             somaMedia += media;
-            System.out.println("Nome: " + nome[i] + " média: " + media );
+            System.out.println("Nome: " + nome[j] + " média: " + media );
         }
-        System.out.println("Média da turma: " + somaMedia/quantidade);
+        System.out.println("Média da turma: " + somaMedia/i);
     }
         
 }

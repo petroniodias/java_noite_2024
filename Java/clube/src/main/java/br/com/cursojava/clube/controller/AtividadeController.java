24d4e21514c126dd8d9dc1be74282a9f1f0b8c6a
petroniodias <63084961+petroniodias@users.noreply.github.com>
2024-07-11 20:38:09 -0300

    Update AtividadeController.java

--- Java/clube/src/main/java/br/com/cursojava/clube/controller/AtividadeController.java
+++ Java/clube/src/main/java/br/com/cursojava/clube/controller/AtividadeController.java
@@ -3,8 +3,11 @@ package br.com.cursojava.clube.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,17 +18,43 @@ import br.com.cursojava.clube.repository.AtividadeRepository;
 @RestController
 @RequestMapping("/atividade")
 public class AtividadeController {
-    
+
     @Autowired
     private AtividadeRepository repositorio;
-    
+
     @GetMapping
-    public List<Atividade> listar(){
+    public List<Atividade> listar() {
         return repositorio.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Atividade listarId(@PathVariable Long id) {
+        return repositorio.findById(id).orElse(null);
+    }
+
     @PostMapping
-    public Atividade adicionar(@RequestBody Atividade atividade){
+    public Atividade adicionar(@RequestBody Atividade atividade) {
         return repositorio.save(atividade);
+    }
+
+    @PutMapping("/{id}")
+    public Atividade alterar(@PathVariable Long id, @RequestBody Atividade atividade) {
+        Atividade atividadeExistente = repositorio.findById(id).orElse(null);
+        if (atividadeExistente != null) {
+            atividadeExistente.setNome(atividade.getNome());
+            atividadeExistente.setDescricao(atividade.getDescricao());
+            return repositorio.save(atividadeExistente);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public String excluir(@PathVariable Long id) {
+        Atividade atividadeExistente = repositorio.findById(id).orElse(null);
+        if (atividadeExistente != null) {
+            repositorio.delete(atividadeExistente);
+            return "Atividade excluída com sucesso";
+        }
+        return "Atividade não localizada";
     }
 }
